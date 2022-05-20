fc663263341a5a529d41611b6c5da9be31287b92
James Copeland <james.copeland.m@gmail.com>
2022-05-19 21:37:16 -0400

    Add update modal

--- App/dashboard.js
+++ App/dashboard.js
@@ -1,5 +1,8 @@
 /* globals Chart:false, feather:false */
 const baseURL = "https://trackableapi.azurewebsites.net";
+var newTaskForm = document.getElementById("newTaskForm")
+var updateTaskForm = document.getElementById("updateTaskForm")
+var taskObject = {}
 
 function buildTable(data) {
   var table = document.getElementById("task-table-body")
@@ -11,7 +14,7 @@ function buildTable(data) {
     buildCell(2, row, element.message)
     buildCell(3, row, element.status)
     buildCell(4, row, element.type)
-    buildCell(5, row, `<button type="button" class="btn btn-secondary edit-btn" onclick="editTask(this, ${element.taskId})">Edit</button>`, true)
+    buildCell(5, row, `<button type="button" class="btn btn-secondary edit-btn" onclick="openUpdateModal(this, ${element.taskId}, ${i})">Edit</button>`, true)
     buildCell(6, row, `<button type="button" class="btn btn-danger delete-btn" onclick="deleteTask(this, ${element.taskId})">Delete</button>`, true)
 
   });
@@ -25,8 +28,6 @@ function buildCell(i, row, text, isBtnCell) {
     cell.classList.add('btn-cell')
   }
 }
-
-var newTaskForm = document.getElementById("newTaskForm")
 
 newTaskForm.addEventListener('submit', function(event) {
   event.preventDefault();
@@ -45,6 +46,42 @@ newTaskForm.addEventListener('submit', function(event) {
     "Trace": ""
   })
 })
+
+updateTaskForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  event.stopPropagation();
+
+  var name = document.getElementById("update-task-name").value
+  var message = document.getElementById("update-message-text").value
+  var status = document.getElementById("update-task-status").value
+  var type = document.getElementById("update-task-type").value
+  var id = document.getElementById("update-task-id").value
+
+  updateTask(id, {
+    "Name": name,
+    "Message": message,
+    "Status": status,
+    "Type": type,
+    "Trace": ""
+  })
+})
+
+function openUpdateModal(element, id, rowNum) {
+
+  var name = taskObject[rowNum].name
+  var message = taskObject[rowNum].message
+  var status = taskObject[rowNum].status
+  var type = taskObject[rowNum].type
+
+  document.getElementById("update-task-name").value = name;
+  document.getElementById("update-message-text").value = message;
+  document.getElementById("update-task-status").value = status;
+  document.getElementById("update-task-type").value = type;
+  document.getElementById("update-task-id").value = id;
+
+  $('#updateTaskModal').modal('show')
+
+}
 
 function createTask(newTask) {
 
@@ -72,6 +109,32 @@ function createTask(newTask) {
 
 }
 
+function updateTask(id, updatedTask) {
+
+  const endpoint = `/Tasks/${id}`
+
+  const headers = new Headers();
+  headers.append("Content-Type", "application/json")
+  headers.append("Access-Control-Allow-Origin", "*")
+
+  const options = {
+    method: "PUT",
+    headers: headers,
+    body: JSON.stringify(updatedTask)
+  }
+  
+  return (passTokenToApi(endpoint, options, headers)
+  .then(putResponse => {
+    getTasks().then(function(data) {
+      clearTable()
+      buildTable(data)
+      $("#updateTaskModal").modal('hide')
+      updateTaskForm.reset()
+    })
+  }))
+
+}
+
 function getTasks() {
 
   const endpoint = "/Tasks"
@@ -85,8 +148,15 @@ function getTasks() {
     headers: headers
   }
 
-  return passTokenToApi(endpoint, options, headers)
-
+  return (passTokenToApi(endpoint, options, headers)
+    .then(data => {
+      taskObject = data;
+    })
+    .catch(error => {
+      taskObject = {}
+      throw error;
+    })
+  )
 }
 
 function clearTable() {
